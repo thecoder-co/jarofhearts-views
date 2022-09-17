aba91ea5c1fabc25cff503a251078e59c044a170
thecoder-co <thecoder.co@gmail.com>
2022-09-17 11:18:10 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jar of Hearts',
       theme: ThemeData(
         fontFamily: 'Dys',
         primarySwatch: Colors.blue,
